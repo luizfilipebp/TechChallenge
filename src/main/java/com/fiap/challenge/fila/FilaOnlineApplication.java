51f09fb42642776fabbb7a31207fba06bd055327
Luiz Filipe <luizfilipe098@gmail.com>
2024-05-11 14:59:14 -0300

    initial commit

---
+++ src/main/java/com/fiap/challenge/fila/FilaOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge.fila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/challenge/fila/FilaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge.fila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
